c79165f6ce5e68b7afa8496ce7c2769766aa6d51
swalehk <118256935+swalehk@users.noreply.github.com>
2022-11-22 19:11:45 +0300

    Add files via upload

---
+++ TASK THREE TWO d.java
@@ -0,0 +1,22 @@
+package myoutput;
+
+/**
+ *
+ * @author Admin
+ */
+public class MyOutPut {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   
+	        { 
+	             StringBuffer bfobj = new StringBuffer("Jambo");
+             StringBuffer bfobj1 = new StringBuffer(" Kenya");
+	             append(bfobj1);
+	             System.out.println(bfobj);
+	        }
+}
+
+    }
